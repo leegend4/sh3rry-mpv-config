91caf1d3e87a698683e4995c42280594b14a3c40
sh3rry <sh3rry@qq.com>
2016-11-02 22:53:34 +0800

    reduce cpu stress

--- mpv.app/Contents/Resources/scripts/autosub.lua
+++ mpv.app/Contents/Resources/scripts/autosub.lua
@@ -2,18 +2,16 @@
 -- default keybinding: b
 -- add the following to your input.conf to change the default keybinding:
 -- keyname script_binding auto_load_subs
-
 local utils = require 'mp.utils'
 function load_sub_fn()
     subl = "/usr/local/bin/subliminal" -- use 'which subliminal' to find the path
     mp.msg.info("Searching subtitle")
     mp.osd_message("Searching subtitle")
     t = {}
-    t.args = {subl, "download", "-l", "en", "-l", "eng", "-p", "podnapisi", "-p", "addic7ed", "-p", "subscenter", "-p", "thesubdb", "-p", "tvsubtitles", mp.get_property("path")}
+    t.args = {subl, "download", "-s", "-l", "en", mp.get_property("path")}
     res = utils.subprocess(t)
-    print(res)
     if res.status == 0 then
-        mp.commandv("rescan_external_files", "reselect")
+        mp.commandv("rescan_external_files", "reselect") 
         mp.msg.info("Subtitle download succeeded")
         mp.osd_message("Subtitle download succeeded")
     else
@@ -22,4 +20,4 @@ function load_sub_fn()
     end
 end
 
-mp.add_key_binding("Alt+S", "auto_load_subs", load_sub_fn)
+mp.add_key_binding("b", "auto_load_subs", load_sub_fn)
